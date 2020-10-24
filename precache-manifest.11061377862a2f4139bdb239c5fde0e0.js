903548753a607f615105456c1022a9ee97b45185
IG(Ingyum) Kim <59094101+IG-Kim2511@users.noreply.github.com>
2020-10-24 15:04:12 -0400

    Add files via upload

---
+++ precache-manifest.11061377862a2f4139bdb239c5fde0e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283db054ba2f29a0f8c322f241d23945",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1214eab0458318b65fc5",
+    "url": "/static/css/main.294ecc29.chunk.css"
+  },
+  {
+    "revision": "d56da393b21108c32758",
+    "url": "/static/js/2.d2b46133.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d2b46133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1214eab0458318b65fc5",
+    "url": "/static/js/main.44676dcd.chunk.js"
+  },
+  {
+    "revision": "c672d0d3653d3911a046",
+    "url": "/static/js/runtime-main.d751eeb8.js"
+  }
+]);
